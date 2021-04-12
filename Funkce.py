5211f860d9f4d19e8f2166836fc3fde56e0b9636
MickiZ <mich.mal@seznam.cz>
2021-04-12 12:22:49 +0200

    Souboj v samostaném modulu

--- Funkce.py
+++ Funkce.py
@@ -1,4 +1,15 @@
+import random
 from random import randrange
+
+# 0. jméno, 1. útok, 2. obrana, 3. životy, 4. xp, 5. loot
+krysa = ["Krysa", 1, 0, 10, 50,[["krysí ocas", 0, 1, False, "item"], ["krysí zub", 0, 1, False, "item"], ["krysí bobek", 0, 1, False, "item"]]]
+bandita = ["Bandita", 10, 3, 30, 60,[["bandití ocas", 0, 1, False, "item"], ["bandití zub", 0, 1, False, "item"], ["bandití bobek", 0, 1, False, "item"]]]
+medved = ["Medvěd", 8, 2, 15, 40,[["medvědí ocas", 0, 1, False, "item"], ["medvědí zub", 0, 1, False, "item"], ["medvědí bobek", 0, 1, False, "item"]]]
+#nepritel = [krysa, medved, bandita]
+#inventory = []
+#xp = 0
+
+
 def boj(ut, ob, utN, obN):
     var_ob_player = ob + randrange(0, 6)
     var_ob_enemy = obN + randrange(0,6)
@@ -10,7 +21,7 @@ def boj(ut, ob, utN, obN):
         zraneni_enemy = 0
     return (zraneni_enemy, zraneni_player)
 
-def level_up(xp,hp,atck,lvl):
+def level_up(xp,hp,atck,lvl,):
     print("Gratuluji získal jsi level.")
     atck += 1
     hp += 5
@@ -19,3 +30,33 @@ def level_up(xp,hp,atck,lvl):
     print(f"Aktuálně máš {lvl}. úroveň, útok: {atck}, {hp} životů a {xp} zkušeností.")
     return (atck, hp, lvl, xp)
 
+def fight(enemy_list, player_hp, player_atc, player_def, player_name, player_xp, player_inventory):
+    get_enemy = random.choice(enemy_list)
+    enemy_name = get_enemy[0]
+    print(f"{player_name} bojuje proti {get_enemy[0]}")
+    enemy_hp = get_enemy[3]
+    enemy_atc = get_enemy[1]
+    enemy_def = get_enemy[2]
+    enemy_loot = get_enemy[5][randrange(0, len(get_enemy[5]))]
+    enemy_xp = get_enemy[4]
+    xp = player_xp
+    
+    while player_hp > 0:
+        if enemy_hp > 0:
+            zraneni_enemy, zraneni_player = boj(player_atc, player_def, enemy_atc, enemy_def)
+            print(f"Zranění způsobené nepřítely je {zraneni_enemy}")
+            print(f"Zranění způsobené hráči je {zraneni_player}")
+            player_hp -= zraneni_player
+            enemy_hp -= zraneni_enemy
+            print(f"Máš {player_hp} životů") 
+            print(f"{enemy_name} má {enemy_hp} životů")
+        else:
+            print("Vyhrál jsi!")
+            xp = player_xp + enemy_xp
+            player_inventory.append(enemy_loot)
+            print(f"Získal jsi {enemy_loot[0]}")
+            break
+    else:
+        print("Prohrál jsi!")
+    return(player_hp, xp, player_inventory)
+#fight(nepritel, 50, 10, 0, "Walak", xp, inventory)
--- Hra.py
+++ Hra.py
@@ -18,8 +18,8 @@ lvl = 1
 
 # 0. jméno, 1. útok, 2. obrana, 3. životy, 4. xp, 5. loot
 krysa = ["Krysa", 1, 0, 10, 50,[["krysí ocas", 0, 1, False, "item"], ["krysí zub", 0, 1, False, "item"], ["krysí bobek", 0, 1, False, "item"]]]
-bandita = ["Bandita", 10, 3, 30]
-medved = ["Medvěd", 8, 2, 15]
+bandita = ["Bandita", 10, 3, 30, 60,[["bandití ocas", 0, 1, False, "item"], ["bandití zub", 0, 1, False, "item"], ["bandití bobek", 0, 1, False, "item"]]]
+medved = ["Medvěd", 8, 2, 15, 40,[["medvědí ocas", 0, 1, False, "item"], ["medvědí zub", 0, 1, False, "item"], ["medvědí bobek", 0, 1, False, "item"]]]
 nepritel = [krysa]
 
 
@@ -27,11 +27,13 @@ nepritel = [krysa]
 
 
 while zivoty > 0:
-    if xp >= 100: #Zisk nového lvl
+    #Zisk nového lvl
+    if xp >= 100: 
        (utok, zivoty, lvl, xp) = Funkce.level_up(xp, zivoty, utok, lvl)
 
     print(f"""Tvůj hrdina se jmenuje {jmeno_hrdiny}, je na {lvl}. úrovni. aktuálně má {zivoty} životů z maximálních {default_hp} životů, {penize} peněz a {xp} zkušeností.
     Nachází se ve městě Lotaru. Je zde kovárna a aréna""")
+
     cesta = input("Kam chceš jít? (napiš forge nebo arena nebo inv) ")
     if cesta == 'inv':
     	Inventář.inventory(inventory_list, equiped)
@@ -42,29 +44,10 @@ while zivoty > 0:
         
     elif cesta == "arena":
         print("Vítej v aréně!")
-        get_nepritel = random.choice(nepritel)
-        print(f"{jmeno_hrdiny} bojuje proti {get_nepritel[0]}")
-        #Tady začíná souboj 
-        zivoty_nepritel = get_nepritel[3]
-        while zivoty > 0:
-            if zivoty_nepritel > 0:
-                zraneni_enemy, zraneni_player = Funkce.boj(utok, obrana, get_nepritel[1], get_nepritel[2])
-                print(f"Zranění nepřítele je {zraneni_enemy}")
-                print(f"Zranění hráče je {zraneni_player}")
-                zivoty = zivoty - zraneni_player
-                zivoty_nepritel = zivoty_nepritel - zraneni_enemy
-                print(f"Máš {zivoty} životů") 
-                print(f"{get_nepritel[0]} má {zivoty_nepritel} životů")
-            else: 
-                print("Vyhrál jsi!")
-                penize = penize + 100
-                xp = xp + get_nepritel[4]
-                loot = get_nepritel[5][randrange(0, len(get_nepritel[5]))]
-                inventory_list.append(loot)
-                print(f"Získal jsi: {loot[0]}!")
-                break
-        else:
-            print("Prohrál jsi!")
+
+        (zivoty, xp, inventory_list) = Funkce.fight(nepritel, zivoty, utok, obrana, jmeno_hrdiny, xp, inventory_list)
+
+        
     # Tady začíná souboj
     else:
         print("Nerozumím! Posral jsi to!")
